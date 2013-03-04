e69d95543e78e1efeb1fe0e73f9d177d14fb23d6
Sergey Grigorchuk <grigorchuk@gmail.com>
2013-03-04 18:47:22 +0400

    Timezone detection

--- wonder.py
+++ wonder.py
@@ -2,6 +2,7 @@
 import json
 from flask import Flask, render_template, request
 app = Flask(__name__)
+from datetime import timedelta
 import ephem
 
 @app.route("/")
@@ -18,13 +19,14 @@ def hello():
 def get_long_lat():	
 	lat = str(request.form.get('lat',''))
 	lon = str(request.form.get('lon',''))
+	delta = timedelta(minutes = int(request.form.get('offset','')))
 	observer = ephem.Observer()
 	observer.lon, observer.lat = lon,lat
 	sun = ephem.Sun()
 	context = {
-	    'sunrise' : str(ephem.localtime( observer.next_rising(sun))),
-	    'sunset'  : str(ephem.localtime(observer.next_setting(sun))),
-	    'log' : str(request.headers)
+	    'sunrise' : str(observer.next_rising(sun).datetime() + delta),
+	    'sunset'  : str(observer.next_setting(sun).datetime() + delta),
+	    'log' : str(ephem.localtime(observer.next_rising(sun)))
 	}
 	return json.dumps(context)
 
