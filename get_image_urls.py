247a427bb99152295a219611ed23f17ea76aa529
Sergey Grigorchuk <grigorchuk@gmail.com>
2014-07-30 02:49:06 +0400

    fixes

--- get_image_urls.py
+++ get_image_urls.py
@@ -10,13 +10,14 @@ month = today.strftime('%B')
 theme = u' природа'.encode('utf8')
 query = month + theme
 
-url = u'https://www.googleapis.com/customsearch/v1?key=AIzaSyC_QleQslgxwueMKYjMSL6leub_Gst1mgc&cx=010367588730355710032:i4pxsroxu6i&{}&imgSize=huge&searchType=image'.format(urlencode({'q': query}))
-links = []
+url = u'https://www.googleapis.com/customsearch/v1?key=AIzaSyC_QleQslgxwueMKYjMSL6leub_Gst1mgc&cx=010367588730355710032:i4pxsroxu6i&{}&imgSize=huge&searchType=image&safe=high'.format(urlencode({'q': query}))
 
-for i in range(10):
-    r = requests.get(url)
-    search_results = r.json()
-    links += [r['link'] for r in search_results['items']]
+r = requests.get(url)
+search_results = r.json()
 
-with open('image_search_results.json', 'w') as f:
-    json.dump(list(set(links)), f)
+try:
+    links = [r['link'] for r in search_results['items']]
+    with open('image_search_results.json', 'w') as f:
+        json.dump(list(set(links)), f)
+except KeyError as e:
+    print search_results
