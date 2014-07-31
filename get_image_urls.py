98531637cef7e29d81e3aa5473662783b3fc2ace
Madsaylor <grigorchuk@gmail.com>
2014-07-31 15:13:07 +0400

    extracting all links

--- get_image_urls.py
+++ get_image_urls.py
@@ -9,15 +9,23 @@ today = date.today()
 month = today.strftime('%B')
 theme = u' природа'.encode('utf8')
 query = month + theme
+query_params = {'q': query, 
+				'imgSize': 'huge',
+				'safe': 'medium',
+				'searchType':'image'}
 
-url = u'https://www.googleapis.com/customsearch/v1?key=AIzaSyC_QleQslgxwueMKYjMSL6leub_Gst1mgc&cx=010367588730355710032:i4pxsroxu6i&{}&imgSize=huge&searchType=image&safe=high'.format(urlencode({'q': query}))
+links = []
+for i in range(1,99):
+	query_params['start'] = i
+	url = u'https://www.googleapis.com/customsearch/v1?key=AIzaSyC_QleQslgxwueMKYjMSL6leub_Gst1mgc&cx=010367588730355710032:i4pxsroxu6i&{}'.format(urlencode(query_params))
+	r = requests.get(url)
+	search_results = r.json()
+	try:
+		links += [r['link'] for r in search_results['items']]
+	except KeyError:
+		print search_results, url
 
-r = requests.get(url)
-search_results = r.json()
+print len(set(links))
 
-try:
-    links = [r['link'] for r in search_results['items']]
-    with open('image_search_results.json', 'w') as f:
-        json.dump(list(set(links)), f)
-except KeyError as e:
-    print search_results
+with open('image_search_results.json', 'w') as f:
+	json.dump(list(set(links)), f)
